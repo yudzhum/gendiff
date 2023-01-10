f9c2c8326d1217c8cb6e96a17bac1489c113adf7
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-01-10 16:20:49 +0300

    Refactor gendiff

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,11 +1,14 @@
 import argparse
 
 
-def parser():
+def get_arguments():
+    """Parse arguments"""
+
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', choices=['stylish', 'plain', 'json'],
+                        default='stylish', help='set format of output')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
 
 
-import argparse
 from gendiff.gendiff import generate_diff
+from gendiff.cli import get_arguments
 
 
 def main():
     """Take two files and show diff"""
 
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-    parser.add_argument('-f', '--format', choices=['stylish', 'plain', 'json'],
-                        default='stylish', help='set format of output')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    args = parser.parse_args()
-
+    args = get_arguments()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
 
