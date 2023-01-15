f4ee381df99db1ff19d6a2dcbf1728508a2f1d46
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-01-15 19:31:35 +0300

    refactor code

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,24 +1,27 @@
 from itertools import chain
 
 
+# Intends with status of values
 INDENT = '    '
-
 ADDED = '  + '
 REMOVED = '  - '
-UNCHANGED = '    '
-UPDATED = '  - '
 
+
+# Statuses of nodes and values
 STATUS = {
     'added': ADDED,
     'removed': REMOVED,
     'unchanged': INDENT,
+    'nothing': INDENT,
+    'nested': INDENT
 }
 
 
 def to_str(value):
     """
     Convert values:
-    False -> false, True -> true, None -> null
+    False -> false, True -> true, None -> null,
+    all -> string
     """
     if value is None:
         return 'null'
@@ -28,37 +31,46 @@ def to_str(value):
 
 
 def stylish(tree):
+    """Take diff tree and return formatted string"""
 
     def iter_(node, depth):
 
+        # Case of primitive value
         if not isinstance(node, dict):
             return to_str(node)
 
-        current_indent = INDENT * depth
+        cur_indent = INDENT * depth
         lines = []
         for key, value in node.items():
+            # Case of primitive value
             if not isinstance(value, dict):
-                lines.append(f'{current_indent}{INDENT}{key}: {to_str(value)}')
-
-            elif value.get('type') == 'nested':
-                children = value.get('children')
-                lines.append(f'{current_indent}{INDENT}{key}: {iter_(children, depth + 1)}')
+                lines.append(f'{cur_indent}{INDENT}{key}: {to_str(value)}')
 
             elif value.get('type') == 'updated':
                 old_val = value.get('value')
                 new_val = value.get('changed_value')
-                lines.append(f'{current_indent}{REMOVED}{key}: {iter_(old_val, depth + 1)}')
-                lines.append(f'{current_indent}{ADDED}{key}: {iter_(new_val, depth + 1)}')
+                lines.append(
+                    f'{cur_indent}{REMOVED}{key}: {iter_(old_val, depth + 1)}'
+                )
+                lines.append(
+                    f'{cur_indent}{ADDED}{key}: {iter_(new_val, depth + 1)}'
+                )
 
-            elif value.get('type'):
-                val = value.get('value')
-                status = value.get('type')
-                lines.append(f'{current_indent}{STATUS[status]}{key}: {iter_(val, depth + 1)}')
-
+            # Case of types: nested, added, unchanged, removed
+            # and dict_value
             else:
-                lines.append(f'{current_indent}{INDENT}{key}: {iter_(value, depth + 1)}')
+                status = value.get('type', 'nothing')
+                deep_level = {
+                    'nested': value.get('children'),
+                    'nothing': value,
+                }
+                val = deep_level.get(status, value.get('value'))
+                lines.append(
+                    f'{cur_indent}{STATUS[status]}'
+                    f'{key}: {iter_(val, depth + 1)}'
+                )
 
-        result = chain("{", lines, [current_indent + "}"])
+        result = chain("{", lines, [cur_indent + "}"])
         return '\n'.join(result)
 
     return iter_(tree, 0)
